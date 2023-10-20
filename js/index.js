456882727874c675099e32393cfbfa4b05c75861
“lww0820” <468456671@qq.com>
2023-10-20 16:55:37 +0800

    '实现销售进度/地图/排行'

--- js/index.js
+++ js/index.js
@@ -249,8 +249,183 @@
 
 // 销售额逻辑
 (function(){
+ 
+
+ 
+
+  /***
+   * 1. 下载echarts (完成)
+   * 2. 引入echarts (完成)
+   * 3. 创建渲染的画布 (完成)
+   * 4. 实例化echarts, 并指定渲染的画布   (完成)
+   * 5. 创建配置项  (完成)
+   * 6. 渲染数据
+   * 7. 设置图表自适应
+   */
+
+ 
+
+  const line = document.querySelector(".line")
+  const echartsInstance = echarts.init(line)
+  
+  const data = {
+    year: {
+      info: [
+        '2099年',
+        '2199年',
+        '2299年',
+        '2399年',
+        '2499年',
+        '2599年',
+        '2699年',
+        '2799年',
+        '2899年',
+        '2999年',
+        '3099年',
+        '3199年'
+      ],
+      detail: [
+        [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+        [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79]
+      ]
+    },
+  
+    quarter: {
+      info: ['1季度', '2季度', '3季度', '4季度'],
+      detail: [
+        [23, 75, 12, 97],
+        [43, 31, 65, 23]
+      ]
+    },
+  
+    month: {
+      info: [
+        '1月',
+        '2月',
+        '3月',
+        '4月',
+        '5月',
+        '6月',
+        '7月',
+        '8月',
+        '9月',
+        '10月',
+        '11月',
+        '12月'
+      ],
+      detail: [
+        [34, 87, 32, 76, 98, 12, 32, 87, 39, 36, 29, 36],
+        [56, 43, 98, 21, 56, 87, 43, 12, 43, 54, 12, 98]
+      ]
+    },
+  
+    week: {
+      info: ['近1周', '近2周', '近3周', '近4周', '近5周', '近6周'],
+      detail: [
+        [43, 73, 62, 54, 91, 54, 84, 43, 86, 43, 54, 53],
+        [32, 54, 34, 87, 32, 45, 62, 68, 93, 54, 54, 24]
+      ]
+    }
+  };
+  
+  const option = {
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      data: ['预期销售额', '实际销售额'],
+      // 图例的文字颜色
+      textStyle: {
+        color: '#4c9bfd'
+      },
+      // 设置图例距离右侧10%的间距
+      right: '10%'
+    },
+    // 网格
+    grid: {
+      top: '20%',
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      // 是否显示网格边框
+      show: true,
+      // 设置边框颜色
+      borderColor: '#012f4a',
+      // 是否显示在刻度轴内
+      containLabel: true
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: data.year.info,
+      // x刻度轴显示与隐藏
+      axisTick: {
+        show: false
+      },
+      // x刻度轴文本的颜色
+      axisLabel: {
+        color: '#4c9bfd' // 文本颜色
+      },
+      //去除 x 轴线 
+      axisLine: {
+        show: false // 去除轴线
+      }
+    },
+    yAxis: {
+      type: 'value',
+      axisTick: {
+        show: false // 去除刻度
+      },
+      axisLabel: {
+        color: '#4c9bfd' // 文字颜色
+      },
+      splitLine: {
+        lineStyle: {
+          color: '#012f4a'
+        }
+      }
+    },
+    color: ['#00f2f1', '#ed3f35'],
+    series: [
+      {
+        name: '预期销售额',
+        type: 'line',
+        stack: 'Total',
+        // 折线修饰为圆滑
+        smooth: true,
+        data: data.year.detail[0]
+      },
+      {
+        name: '实际销售额',
+        type: 'line',
+        stack: 'Total',
+        // 折线修饰为圆滑
+        smooth: true,
+        data: data.year.detail[1]
+      }
+    ]
+  }
+
+  echartsInstance.setOption(option)
+  window.addEventListener("resize", () => {
+    echartsInstance.resize()
+  })
+
+  
+
+  $(".caption span").click(function(){
+    const index = ($(this).index()) - 1
+    render(index)
+    
+  })
+
   function render(index){
     $(".caption span").eq(index).addClass("active").siblings("span").removeClass("active")
+    const item = data[$(".caption span").get(index).dataset.index]
+    option.xAxis.data = item.info
+    option.series[0].data = item.detail[0]
+    option.series[1].data = item.detail[1]
+    echartsInstance.setOption(option)
   }
 
   let i = 0
@@ -273,93 +448,309 @@
     autoToggle()
   })
 
-  /***
+})();
+
+// 渠道分布逻辑
+(function(){
+  /**
    * 1. 下载echarts (完成)
    * 2. 引入echarts (完成)
    * 3. 创建渲染的画布 (完成)
-   * 4. 实例化echarts, 并指定渲染的画布   (完成)
-   * 5. 创建配置项  (完成)
-   * 6. 渲染数据
-   * 7. 设置图表自适应
+   * 4. 实例化echarts并指定渲染的画布 (完成)
+   * 5. 设置配置项
+   * 6. 渲染echarts图表
+   * 7. 实现图表自适应
    */
+  const radar = document.querySelector(".radar")
+  const echartsInstance = echarts.init(radar)
+  const dataBJ = [[90, 19, 56, 11, 34]];
 
-  const line = document.querySelector(".line")
-  const echartsInstance = echarts.init(line)
+
   const option = {
-   
     tooltip: {
-      trigger: 'axis'
+      show: true,
+      // 控制提示框组件的显示位置
+      position: ['30%', '10%'],
     },
-    legend: {
-      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      top : '3%',
-      containLabel: true
-    },
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-      type: 'value'
+    radar: {
+        center: ['50%', '50%'],
+        // 外半径占据容器大小
+        radius: '50%',
+      indicator: [
+        { name: '淘宝', max: 90 },
+        { name: '京东', max: 22 },
+        { name: '苏宁', max: 75 },
+        { name: '微商', max: 22 },
+        { name: '其他', max: 132 }
+      ],
+      shape: 'circle',
+      splitNumber: 4,
+      name: {
+        // 修饰雷达图文本颜色
+        textStyle: {
+          color: '#4c9bfd'
+        }
+      },
+    
+      splitLine: {
+        lineStyle: {
+          color: 'rgba(255, 255, 255, 0.5)'
+        }
+      },
+      splitArea: {
+        show: false
+      },
+      axisLine: {
+        lineStyle: {
+          color: 'rgba(255, 255, 255, 0.5)'
+        }
+      }
     },
     series: [
       {
-        name: 'Email',
-        type: 'line',
-        stack: 'Total',
-        data: [120, 132, 101, 134, 90, 230, 210]
-      },
-      {
-        name: 'Union Ads',
-        type: 'line',
-        stack: 'Total',
-        data: [220, 182, 191, 234, 290, 330, 310]
+        name : '上海',
+        type: 'radar',
+        lineStyle: {
+          normal: {
+              color: '#fff',
+              // width: 1
+          }
+        }, 
+        areaStyle: {
+          color: 'rgba(238, 197, 102, 0.6)',
+        },
+        data: dataBJ,
+        symbol: 'circle',
+        // 拐点的大小  
+        symbolSize: 5, 
+        itemStyle: {
+        color : "#fff"
+        } 
       }
     ]
-  };
+  }
+
   echartsInstance.setOption(option)
   window.addEventListener("resize", () => {
     echartsInstance.resize()
   })
+})();
 
-  var data = {
-    // 年
-    year: [
-      [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
-      [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79]
-    ],
-    // 季
-    quarter: [
-      [23, 75, 12, 97, 21, 67, 98, 21, 43, 64, 76, 38],
-      [43, 31, 65, 23, 78, 21, 82, 64, 43, 60, 19, 34]
-    ],
-    // 月
-    month: [
-      [34, 87, 32, 76, 98, 12, 32, 87, 39, 36, 29, 36],
-      [56, 43, 98, 21, 56, 87, 43, 12, 43, 54, 12, 98]
-    ],
-    // 周
-    week: [
-      [43, 73, 62, 54, 91, 54, 84, 43, 86, 43, 54, 53],
-      [32, 54, 34, 87, 32, 45, 62, 68, 93, 54, 54, 24]
-    ]
-  }
+// 销售进度逻辑
 
-  $(".caption span").click(function(){
-    const index = ($(this).index()) - 1
-    render(index)
-    const item = data[this.dataset.index]
-    console.log("item",item)
+(function(){
+    /**
+     * 1. 下载echarts (完成)
+     * 2. 引入echarts (完成)
+     * 3. 创建渲染的画布 (完成)
+     * 4. 实例化echarts并指定渲染的画布 (完成)
+     * 5. 设置配置项 (完成)
+     * 6. 渲染echarts图表 (完成)
+     * 7. 实现图表自适应 (完成)
+     */
 
-    option.series[0].data = item[0]
-    option.series[1].data = item[1]
+    const gauge=document.querySelector('.gauge')
+    const echartsInstance=echarts.init(gauge)
+
+    const option={
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: ['130%', '150%'],
+          center: ['48%', '80%'], 
+          avoidLabelOverlap: false,
+          label: {
+            show: false,
+            position: 'center'
+          },
+          // 图表进行旋转
+          startAngle : 180,
+          hoverOffset: 0,
+          data: [
+            {
+              value: 100,
+              itemStyle: {
+                color: {
+                  type: 'linear',
+                  x: 0,
+                  y: 0,
+                  x2: 0,
+                  y2: 1,
+                  colorStops: [
+                    {
+                      offset: 0,
+                      color: '#00c9e0' // 0% 处的颜色
+                    },
+                    {
+                      offset: 1,
+                      color: '#005fc1' // 100% 处的颜色
+                    }
+                  ],
+                  global: false // 缺省为 false
+                }
+              }
+            },
+            {
+              value: 100,
+              itemStyle: {
+                color: '#12274d'
+              }
+            },
+            { value: 200, itemStyle: { color: 'transparent' } }
+          ]
+        }
+      ]
+    };
+
     echartsInstance.setOption(option)
-  })
-  
+
+    window.addEventListener('resize',()=>{
+      echartsInstance.resize()
+    })
+})();
+
+// 排行逻辑
+(function(){
+  // 1. 准备相关数据
+  var hotData = [
+    {
+      city: "北京", // 城市
+      sales: "25, 179", // 销售额
+      flag: true, //  上升还是下降
+      brands: [
+        //  品牌种类数据
+        { name: "可爱多", num: "9,086", flag: true },
+        { name: "娃哈哈", num: "8,341", flag: true },
+        { name: "喜之郎", num: "7,407", flag: false },
+        { name: "八喜", num: "6,080", flag: false },
+        { name: "小洋人", num: "6,724", flag: false },
+        { name: "好多鱼", num: "2,170", flag: true }
+      ]
+    },
+    {
+      city: "河北",
+      sales: "23,252",
+      flag: false,
+      brands: [
+        { name: "可爱多", num: "3,457", flag: false },
+        { name: "娃哈哈", num: "2,124", flag: true },
+        { name: "喜之郎", num: "8,907", flag: false },
+        { name: "八喜", num: "6,080", flag: true },
+        { name: "小洋人", num: "1,724", flag: false },
+        { name: "好多鱼", num: "1,170", flag: false }
+      ]
+    },
+    {
+      city: "上海",
+      sales: "20,760",
+      flag: true,
+      brands: [
+        { name: "可爱多", num: "2,345", flag: true },
+        { name: "娃哈哈", num: "7,109", flag: true },
+        { name: "喜之郎", num: "3,701", flag: false },
+        { name: "八喜", num: "6,080", flag: false },
+        { name: "小洋人", num: "2,724", flag: false },
+        { name: "好多鱼", num: "2,998", flag: true }
+      ]
+    },
+    {
+      city: "江苏",
+      sales: "23,252",
+      flag: false,
+      brands: [
+        { name: "可爱多", num: "2,156", flag: false },
+        { name: "娃哈哈", num: "2,456", flag: true },
+        { name: "喜之郎", num: "9,737", flag: true },
+        { name: "八喜", num: "2,080", flag: true },
+        { name: "小洋人", num: "8,724", flag: true },
+        { name: "好多鱼", num: "1,770", flag: false }
+      ]
+    },
+    {
+      city: "山东",
+      sales: "20,760",
+      flag: true,
+      brands: [
+        { name: "可爱多", num: "9,567", flag: true },
+        { name: "娃哈哈", num: "2,345", flag: false },
+        { name: "喜之郎", num: "9,037", flag: false },
+        { name: "八喜", num: "1,080", flag: true },
+        { name: "小洋人", num: "4,724", flag: false },
+        { name: "好多鱼", num: "9,999", flag: true }
+      ]
+    }
+  ];
+  //  2. 根据数据渲染各省热销 sup 模块内容
+  // (1) 遍历 hotData对象
+  var supHTML = "";
+  $.each(hotData, function(index, item) {
+    // console.log(item);
+    supHTML += `<li><span>${item.city}</span><span> ${item.sales} <s class=
+    ${item.flag ? "icon-up" : "icon-down"}></s></span></li>`;
+  });
+  // 把生成的5个小li字符串给 sub dom盒子
+  $(".sup").html(supHTML);
+  // 3. 当鼠标进入 tab 的时候
+  // 鼠标经过当前的小li要高亮显示
+  $(".province .sup").on("mouseenter", "li", function() {
+    index = $(this).index();
+    render($(this));
+  });
+
+  // 声明一个函数 里面设置sup当前小li高亮还有 对应的品牌对象渲染
+  // 这个函数需要传递当前元素进去
+  function render(currentEle) {
+    currentEle
+      .addClass("active")
+      .siblings()
+      .removeClass();
+
+      //  currentEle $(this)
+    // 拿到当前城市的品牌对象
+    // console.log($(this).index());
+    // 可以通过hotData[$(this).index()] 得到当前的城市
+    // console.log(hotData[$(this).index()]);
+    // 我们可以通过hotData[$(this).index()].brands 拿到的是城市对象的品牌种类
+    // console.log(hotData[$(this).index()].brands);
+    // 开始遍历品牌数组
+    var subHTML = "";
+    $.each(hotData[currentEle.index()].brands, function(index, item) {
+      // 是对应城市的每一个品牌对象
+      // console.log(item);
+      subHTML += `<li><span>${item.name}</span><span> ${item.num}<s class=
+    ${item.flag ? "icon-up" : "icon-down"}
+    ></s></span></li>`;
+    });
+    // 把生成的6个小li字符串给 sub dom盒子
+    $(".sub").html(subHTML);
+  }
+  // 4. 默认把第一个小li处于鼠标经过状态
+  var lis = $(".province .sup li");
+  lis.eq(0).mouseenter();
+  // 5 开启定时器
+  var index = 0;
+  var timer = setInterval(function() {
+    index++;
+    if (index >= 5) index = 0;
+    // lis.eq(index).mouseenter();
+    render(lis.eq(index));
+  }, 2000);
+
+  $(".province .sup").hover(
+    // 鼠标经过事件
+    function() {
+      clearInterval(timer);
+    },
+    // 鼠标离开事件
+    function() {
+      clearInterval(timer);
+      timer = setInterval(function() {
+        index++;
+        if (index >= 5) index = 0;
+        // lis.eq(index).mouseenter();
+        render(lis.eq(index));
+      }, 2000);
+    }
+  );
 })();
